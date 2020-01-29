eb5cf913e5b12bbf954aa5883af399f8776c6636
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-01-29 21:28:14 +0530

    settings.py database

--- ees/settings.py
+++ ees/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'ees.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'schooldb',
+        'USER': 'schooladmin',
+        'PASSWORD': 'django@123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'schooldb',
-            'USER': 'schooladmin',
-            'PASSWORD': 'django@123',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
 }    
 
 # DATABASES = {
